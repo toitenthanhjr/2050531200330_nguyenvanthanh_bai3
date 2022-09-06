5a873590885d5368edc8e9667a78121524a9e1ed
Nguyễn Văn Thanh <111990902+toitenthanhjr@users.noreply.github.com>
2022-09-06 08:45:28 +0700

    2050531200330_nguyenvanthanh

---
+++ app/src/main/java/com/example/a2050531200330_nguyenvanthanh_bai3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a2050531200330_nguyenvanthanh_bai3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
